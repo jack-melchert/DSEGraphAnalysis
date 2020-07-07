779099e194d6a5e36f8999999455f182b68eb50b
Jackson Melchert <melchert@stanford.edu>
2020-07-07 11:58:06 -0700

    Reverted to old parsing

--- subgraph_mining/convert_dot.py
+++ subgraph_mining/convert_dot.py
@@ -1,7 +1,6 @@
 import json
 import sys
 import os
-import coreir
 import pickle
 
 def convert_dot(coreir_files):
@@ -28,10 +27,10 @@ def convert_dot(coreir_files):
         
         print(f)
 
-        c = coreir.Context()
-        c.load_library("commonlib")
-        c.load_library("lakelib")
-        mod = c.load_from_file(f)
+        file = open(f, 'r')
+        json_in = json.loads(file.read())
+        json_insts = json_in['namespaces']['global']['modules']['DesignTop']['instances']
+        json_connections = json_in['namespaces']['global']['modules']['DesignTop']['connections']
 
         dot_files = [os.path.basename(f).replace(".json", ".dot") for f in coreir_files]
 
@@ -40,37 +39,38 @@ def convert_dot(coreir_files):
             
             inst_ind = 0
             out_file.write('t # 1\n')
-            for inst in mod.definition.instances:
-                op = inst.module.name
-                name = inst.name
-                if op in supported_ops:
-                    used_ops.add(op)
+            for name, inst in json_insts.items():
+                if 'genref' in inst:
+                    op = inst['genref'].split('.')[1]
 
-                    if op not in op_types:
-                        op_types[op] = op_index
-                        op_index += 1
+                    if op in supported_ops:
+                        used_ops.add(op)
 
-                    out_file.write('v ' + str(inst_ind) + ' ' + str(op_types[op]) + '\n')
-                    instance_names[name] = inst_ind
-                    inst_ind += 1
-                else:
-                    unused_ops.add(op)
+                        if op not in op_types:
+                            op_types[op] = op_index
+                            op_index += 1
 
-
-            for conn in mod.definition.connections:
-                first = conn.first.selectpath[0]
-                second = conn.second.selectpath[0]
-
-                if (first in instance_names and second in instance_names):
-                    node0 = instance_names[first]
-                    node1 = instance_names[second]
-
-                    if conn.first.selectpath[1] == "out":
-                        out_file.write('e ' + str(node0) + ' ' + str(node1) + ' ' + conn.second.selectpath[1].replace('in', '').replace('sel', '2') +'\n')
+                        out_file.write('v ' + str(inst_ind) + ' ' + str(op_types[op]) + '\n')
+                        instance_names[name] = inst_ind
+                        inst_ind += 1
                     else:
-                        out_file.write('e ' + str(node1) + ' ' + str(node0) + ' ' + conn.first.selectpath[1].replace('in', '').replace('sel', '2') +'\n')
+                        unused_ops.add(op)
 
-        print(used_ops)
+
+            for first, second in json_connections:
+                first_name = first.split(".")[0]
+                first_port = first.split(".")[1]
+                second_name = second.split(".")[0]
+                second_port = second.split(".")[1]
+                if (first_name in instance_names and second_name in instance_names):
+                    node0 = instance_names[first_name]
+                    node1 = instance_names[second_name]
+
+                    if first_port == "out":
+                        out_file.write('e ' + str(node0) + ' ' + str(node1) + ' ' + second_port.replace('in', '').replace('sel', '2') +'\n')
+                    else:
+                        out_file.write('e ' + str(node1) + ' ' + str(node0) + ' ' + first_port.replace('in', '').replace('sel', '2') +'\n')
+
 
     with open('.temp/op_types.txt', 'wb') as op_types_out_file:
         # op_types_out_file.write(str(op_types))
